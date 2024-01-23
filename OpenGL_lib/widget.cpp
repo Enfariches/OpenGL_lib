419e822565a601ef697f069672856242eb479504
Enfariches <serg-eburg2@yandex.ru>
2024-01-23 14:30:07 +0500

    fix light and added hole

--- OpenGL_lib/widget.cpp
+++ OpenGL_lib/widget.cpp
@@ -16,7 +16,6 @@ void Widget::initializeGL()
 {
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_LIGHTING);
-    //glEnable(GL_LIGHT0);
     glEnable(GL_LIGHT1);
     glEnable(GL_COLOR_MATERIAL);
     glEnable(GL_NORMALIZE);
